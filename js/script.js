8176f41791c4f7b0bd2656e834307c89ff82e12c
Kerim Trebeß <k.trebess@iasonconsulting.com>
2025-09-11 14:50:25 +0200

    Beautify style.css and script.js.

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ const bar = document.getElementById("progressBar");
 const button = document.getElementById("redirectButton");
 
 document.addEventListener("DOMContentLoaded", () => {
-    bar.addEventListener("animationend", (e) => {
-        if (e.animationName === "fillBar") redirect();
-    });
+	bar.addEventListener("animationend", (e) => {
+		if (e.animationName === "fillBar") redirect();
+	});
 
-    button.addEventListener("click", redirect);
+	button.addEventListener("click", redirect);
 });
 
 function redirect() {
-    try {
-        window.location.href = "idpms://";
-    } catch (error) {
-        console.error(`The forwarding failed, error: ${error}`);
-    }
+	try {
+		window.location.href = "idpms://";
+	} catch (error) {
+		console.error(`The forwarding failed, error: ${error}`);
+	}
 }
