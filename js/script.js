6a7ce597057f708810e750bea9f819ae4d716e23
Kerim Trebeß <k.trebess@iasonconsulting.com>
2025-09-11 09:13:58 +0200

    Add spinner.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
-const protocol = "idpms://";
-const timeout = 1500;
+const customProtocol = "idpms://";
+const redirectDelay = 1500;
 
-function redirectToProtocol() {
+function redirect() {
     console.info("You are being redirected...");
+
+    document.getElementById("loading-overlay").classList.add("active");
     
     try {
-        window.location.href = protocol;
+        window.location.href = customProtocol;
     } catch (error) {
-        console.error(`The forwarding failed. Error: ${error}`);
+        console.error(`The forwarding failed, error: ${error}`);
     }
 
-    window.location.href = protocol;
+    setTimeout(() => {
+        document.getElementById("loading-overlay").classList.remove("active");
+    }, 2 * redirectDelay);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.info(`You will be redirected in ${timeout / 1000} seconds.`);
+    console.info(`You will be redirected in ${redirectDelay / 1000} seconds.`);
 
-    setTimeout(redirectToProtocol, timeout);
+    setTimeout(redirect, redirectDelay);
 });
